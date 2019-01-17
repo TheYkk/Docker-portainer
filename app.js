0037005077c79909c28cd512eea3afec46781dd3
Yusuf Kaan Karakaya <yusufkaan142@gmail.com>
2019-01-17 12:47:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ http.createServer(function (req, res) {
     datetext = datetext.split(' ')[0];
     res.end('HI from Node.js! ' +datetext + ' on ' + process.env.HOSTNAME  + '\n');
     
-}).listen(8080, '0.0.0.0');
-console.log('Server running at http://:8080/');
+}).listen(80, '0.0.0.0');
+console.log('Server running at http://:80/');
